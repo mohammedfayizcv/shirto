959bbd79a494e33048b4e7ca6aa85d7a9b24a119
Mohammed fayiz C v <91322900+mohammedfayizcv@users.noreply.github.com>
2022-03-19 00:21:41 +0530

    Update settings.py

--- shirt_emo_pro/settings.py
+++ shirt_emo_pro/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'shirt_emo_pro.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shirto',
+        'NAME': 'shortodb',
         'USER':'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
